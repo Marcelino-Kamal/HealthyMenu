942dca24426feab487364aafee166b20b7eacc93
Marcelino <marconaoum@gmail.com>
2025-05-07 15:14:22 +0300

    API

--- src/API/MealService.js
+++ src/API/MealService.js
@@ -1,21 +1,20 @@
-// mealService.js
+
 import axios from 'axios';
 
-// Create an Axios instance with your API base URL
 const api = axios.create({
-  baseURL: 'http://localhost:5000/api/meals', // Change this to your actual API base URL
+  baseURL: 'http://localhost:5000/api/meals', 
 });
 
 // Function to fetch all meals
 export const fetchMeals = async (jwtToken) => {
   try {
-    // Make the GET request with JWT token in Authorization header
+    
     const response = await api.get('/', {
       headers: {
         Authorization: `Bearer ${jwtToken}`,
       },
     });
-    return response.data.meals; // Assuming the response has a 'meals' property
+    return response.data.meals; 
   } catch (error) {
     console.error('Error fetching meals:', error);
     throw error;
